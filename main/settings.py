ea8ce1a011417840d9f981c06a72b99c1dc7c902
Luis F. Villalba <villalba.luifer@gmail.com>
2025-11-28 11:03:13 -0300

    Reducing logging noise

--- main/settings.py
+++ main/settings.py
@@ -254,19 +254,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
+    'handlers': {'null': {'class': 'logging.NullHandler'}},
     'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
+        'django.development': {'handlers': ['null']},
+        'django.utils.autoreload': {'handlers': ['null']},
+    }
 }
